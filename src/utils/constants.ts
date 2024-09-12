9008ea2dd95569fe896fb11bcad96720a166d3ae
Artur Ohanyan <arturo.yan10@gmail.com>
2024-09-12 13:54:22 +0400

    added sources

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+const SOURCE_SRC = [
+  "https://github.com/YauhenKavalchuk/interview-questions/blob/main/questions/react.md",
+  "https://itvdn.com/ua/blog/article/300-js",
+];
+
+export { SOURCE_SRC };
